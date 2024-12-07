dbe700ee3f06c6b9677045b21df1bc28246ca368
SachinJamesA <143333095+SachinJamesA@users.noreply.github.com>
2024-12-07 19:45:53 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "flavourFly_Db";
---
+++ src/server.js
@@ -0,0 +1,8 @@
+import connectDB from "./db/index.js";  
+import dotenv from 'dotenv'
+
+connectDB()
+
+dotenv.config({
+    path: "./.env",
+});
