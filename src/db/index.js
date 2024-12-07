f1f6162803af1c609bcf3a1a29dd273a77185e6c
SachinJamesA <143333095+SachinJamesA@users.noreply.github.com>
2024-12-07 20:18:46 +0530

    added mongodb connection and port

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,13 @@
 import mongoose from 'mongoose';
-import { DB_NAME } from '../constants.js';
+import {DB_NAME} from "../constants.js";
 
-const connectDB = async () =>{
+const connectDB = async () => {
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`Mongodb Connected !! DB Host: ${connectionInstance.connection.host}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log("MongoDb connection failed ", error);
-        process.exit(1);
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
     }
 }
 
